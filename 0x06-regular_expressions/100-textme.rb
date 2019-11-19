ea4fbd5599b3408b8b23b3cc6a2b6043b7483a26
Dario Castaño <dario.castano@aim.com>
2019-11-18 22:01:03 -0500

    Fix 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+\w+|\w+)\] \[to:([\+0-9]+)\] \[flags:([:0-9-]+)\]/).join(", ")
+puts ARGV[0].scan(/\[from:(\+\w+|\w+)\] \[to:(\+\w+|\w+)\] \[flags:([:0-9-]+)\]/).join(", ")
