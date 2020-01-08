b81f4623280b217c379cfb62c477621e13340fc0
Dario Castaño <dario.castano@aim.com>
2020-01-08 17:56:00 -0500

    Fix puppet conf

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,19 +1,24 @@
 # Configure a nginx server with header
 
-exec{'update_install_apt':
-  command => "sudo apt-get update && sudo apt-get install -y nginx"
+package{'nginx':
+  name => 'nginx',
+  provider => 'apt',
+  ensure => installed
 }
 
 exec{'download_conf':
-  command => "/usr/bin/wget -q https://raw.githubusercontent.com/dario-castano/holberton-system_engineering-devops/master/0x0F-load_balancer/nginx.conf -O /etc/nginx/nginx.conf",
-  creates => "/etc/nginx/nginx.conf",
+  command => '/usr/bin/wget -q https://raw.githubusercontent.com/dario-castano/holberton-system_engineering-devops/master/0x0F-load_balancer/nginx.conf -O /etc/nginx/nginx.conf',
+  require => Package['nginx']
+}
+
+service{'nginx_service':
+  name => 'nginx',
+  enable => 'true',
+  ensure => 'running',
+  require => [Package['nginx'], Exec['download_conf']]
 }
 
 file{'/etc/nginx/nginx.conf':
-  mode => 0755,
-  require => Exec["download_conf"],
-}
-
-exec{'start_nginx':
-  command => "sudo service nginx start"
+  notify => Service['nginx_service'],
+  require => Package['nginx']
 }
