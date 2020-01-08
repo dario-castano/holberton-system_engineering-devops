420ba8f2a9c74d3f1af656b365d862e95654d0b0
Dario Castaño <dario.castano@aim.com>
2020-01-08 17:10:32 -0500

    Add answer to task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,17 @@
+exec{'update_install_apt':
+  command => "sudo apt-get update && sudo apt-get install -y nginx"
+}
+
+exec{'download_conf':
+  command => "/usr/bin/wget -q https://raw.githubusercontent.com/dario-castano/holberton-system_engineering-devops/master/0x0F-load_balancer/nginx.conf -O /etc/nginx/nginx.conf",
+  creates => "/etc/nginx/nginx.conf",
+}
+
+file{'/etc/nginx/nginx.conf':
+  mode => 0755,
+  require => Exec["download_conf"],
+}
+
+exec{'start_nginx':
+  command => "sudo service nginx start"
+}
