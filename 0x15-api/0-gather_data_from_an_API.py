e0f57b41221688c1ca3af3ab85fa2527be79c84e
Dario Castaño <dario.castano@aim.com>
2020-02-03 15:25:41 -0500

    Initial commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+"""
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    base_url = 'https://jsonplaceholder.typicode.com'
+
+    user = requests.get('{}/users/{}'.format(base_url, user_id))
+    task = requests.get('{}/todos?userId={}'.format(base_url, user_id))
+
+    user_out = user.json()
+    task_out = task.json()
+
+    all_tasks = len(task_out)
+    done_tasks = len([x for x in task_out if x.get('completed') is True])
+
+    print('Employee {} is done with tasks'
+          .format(user_out.get('name')), end='')
+    print('({}/{}):'.format(done_tasks, all_tasks))
+
+    for task in task_out:
+        if task.get('completed') is True:
+            print('\t {}'.format(task.get('title')))
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+# 0x15-api
+This is the 0x15-api repo for Holberton School
