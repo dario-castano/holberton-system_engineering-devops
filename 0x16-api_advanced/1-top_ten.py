42c52012503e8817f368054c8e323b69080a0d8d
Dario Castaño <dario.castano@aim.com>
2020-02-06 15:36:36 -0500

    Add task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Search top ten hottest posts
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Get top posts on reddit
+    """
+    fake_agent = 'Opera/9.80 (Windows NT 6.1 x64; U; en) Presto/2.7.62'
+    base_url = 'http://www.reddit.com/'
+    resource = 'r/{}/hot.json?limit=10'.format(subreddit)
+    req = requests.get(base_url + resource,
+                       headers={'User-Agent': fake_agent})
+
+    if req.status_code != 404:
+        out = req.json()
+        if out.get('data').get('dist') > 0:
+            for post in out.get('data').get('children'):
+                print(post.get('data').get('title'))
+        else:
+            print('None')
